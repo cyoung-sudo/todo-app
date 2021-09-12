26a690d288823787dc86abdae46f497ae288dad6
Christopher Young <christopheryoung@MacBook123.local>
2021-09-12 12:22:08 -0700

    mobile submit testing

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-$(function() {
+$(document).ready(function(){
 	const input = $('input','#input-wrapper')
 	const inputBtn = $('button','#input-wrapper')
+
 	// Check for "enter" keypress
 	input.on('keydown', function(e) {
 		let key = e.which
 		if(key == '13') {
-			alert("testing2")
+			alert("testing")
 			// Check if text is empty
-			// if(input.val() != '') {
-			// 	createTask()
-			// } else {
-			// 	alert("Text field is empty...")
-			// }
+			if(input.val() != '') {
+				createTask()
+			} else {
+				alert("Text field is empty...")
+			}
 		}
 	})
 
 	// Check for button click
-	inputBtn.on('click', function() {
+	inputBtn.on('click tap', function() {
 		alert("testing")
 		// Check if text is empty
-		// if(input.val() != '') {
-		// 	createTask()
-		// } else {
-		// 	alert("Text field is empty...")
-		// }
+		if(input.val() != '') {
+			createTask()
+		} else {
+			alert("Text field is empty...")
+		}
 	})
 
 	// Create "new task" element
